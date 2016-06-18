f00ac99269f4306426b5c8bdc5d12c0d63a30e80
Henry Hollenstain <henry.l.hollenstain@gmail.com>
2016-06-18 15:51:51 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Example api
+
+## Running the example
+
+To run this example, from the root of this project:
+
+```sh
+go run *.go
+```
+
+## Using the bender quote example
+
+```sh
+curl http://127.0.0.1:8080/bender
+```
---
+++ handlers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+
+	"fmt"
+	"net/http"
+	"log"
+	"math/rand"
+
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "You can't even use this api right you meatbags\n")
+}
+
+func Bender(w http.ResponseWriter, r *http.Request) {
+	lines, err := readLines("bender_lines.txt")
+  if err != nil {
+    log.Fatalf("readLines: %s", err)
+  }
+  fmt.Fprint(w, lines[rand.Intn(len(lines))])
+}
